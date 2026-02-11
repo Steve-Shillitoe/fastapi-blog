056ecce64ecf9a4e7ae43d4e05060cb640017a9e
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2026-02-11 20:42:43 +0000

    Add exception handling to get_post function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException, status
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -42,4 +42,4 @@ def get_post(post_id:int):
     for post in posts:
         if post.get("id") == post_id:
             return post
-    return {"error":"post not found"}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
