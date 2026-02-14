354d2d8d336efd056e27c517400524d3ab718ccd
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2026-02-14 12:51:59 +0000

    Add update functionality

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.exceptions import HTTPException as StarletteHTTPException
-from schemas import PostResponse, PostCreate, UserResponse, UserCreate
+from schemas import PostResponse, PostCreate, UserResponse, UserCreate, PostUpdate
 from typing import Annotated
 from sqlalchemy import select
 from sqlalchemy.orm import Session
@@ -155,6 +155,34 @@ def get_post(post_id: int, db: Annotated[Session, Depends(get_db)]):
     if post:
         return post
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+
+#update post
+@app.put("/api/posts/{post_id}", response_model=PostResponse)
+def update_post_full(post_id: int, post_data:PostCreate, db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(Post).where(Post.id == post_id))
+    post = result.scalars().first()
+    # We need to check that the post we wish to update, exists
+    if not post:
+         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    
+    if post_data.user_id != post.user_id:
+        #check does user exist?
+        result = db.execute(select(User).where(User.id == post_data.user_id))
+        user = result.scalars().first()
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="User not found",
+            )
+        
+    post.title = post_data.title
+    post.content = post_data.content
+    post.user_id = post_data.user_id
+
+    db.commit
+    db.refresh(post)
+    return post
 
 
 ## Create Post
