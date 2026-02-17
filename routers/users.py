e130c825dde82aff925db10536611cb2a1fbd90f
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2026-02-17 18:45:05 +0000

    Reorder API end points to fix bug

--- routers/users.py
+++ routers/users.py
@@ -58,102 +58,6 @@ async def create_user(user: UserCreate, db: Annotated[AsyncSession, Depends(get_
     await db.refresh(new_user)
     return new_user
 
-@router.get("/{user_id}", response_model=UserPublic)
-async def get_user(user_id: int, db: Annotated[AsyncSession, Depends(get_db)]):
-    result = await db.execute(select(User).where(User.id == user_id))
-    user = result.scalars().first()
-    if user:
-        return user
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-
-
-@router.get("/{user_id}/posts", response_model=list[PostResponse])
-async def get_user_posts(user_id: int, db: Annotated[AsyncSession, Depends(get_db)]):
-    result = await db.execute(select(User).where(User.id == user_id))
-    user = result.scalars().first()
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="User not found",
-        )
-    result = await db.execute(
-        select(Post)
-        .options(selectinload(Post.author))
-        .where(Post.user_id == user_id)
-        .order_by(Post.date_posted.desc()),
-    )
-    posts = result.scalars().all()
-    return posts
-
-
-@router.patch("/{user_id}", response_model=UserPrivate)
-async def update_user(
-    user_id: int,
-    user_update: UserUpdate,
-    db: Annotated[AsyncSession, Depends(get_db)],
-):
-    result = await db.execute(select(User).where(User.id == user_id))
-    user = result.scalars().first()
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="User not found",
-        )
-    if (
-        user_update.username is not None
-        and user_update.username.lower() != user.username.lower()
-    ):
-        result = await db.execute(
-            select(User).where(
-                func.lower(User.username) == user_update.username.lower(),
-            ),
-        )
-        existing_user = result.scalars().first()
-        if existing_user:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="Username already exists",
-            )
-    if (
-        user_update.email is not None
-        and user_update.email.lower() != user.email.lower()
-    ):
-        result = await db.execute(
-            select(UserPublic).where(
-                func.lower(User.email) == user_update.email.lower(),
-            ),
-        )
-        existing_email = result.scalars().first()
-        if existing_email:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="Email already registered",
-            )
-
-    if user_update.username is not None:
-        user.username = user_update.username
-    if user_update.email is not None:
-        user.email = user_update.email.lower()
-    if user_update.image_file is not None:
-        user.image_file = user_update.image_file
-
-    await db.commit()
-    await db.refresh(user)
-    return user
-
-@router.delete("/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_user(user_id: int, db: Annotated[AsyncSession, Depends(get_db)]):
-    result = await db.execute(select(User).where(User.id == user_id))
-    user = result.scalars().first()
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="User not found",
-        )
-
-    await db.delete(user)
-    await db.commit()
-
 
 @router.post("/token", response_model=Token)
 async def login_for_access_token(
@@ -222,3 +126,100 @@ async def get_current_user(
             headers={"WWW-Authenticate": "Bearer"},
         )
     return user
+
+
+@router.get("/{user_id}", response_model=UserPublic)
+async def get_user(user_id: int, db: Annotated[AsyncSession, Depends(get_db)]):
+    result = await db.execute(select(User).where(User.id == user_id))
+    user = result.scalars().first()
+    if user:
+        return user
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+
+@router.get("/{user_id}/posts", response_model=list[PostResponse])
+async def get_user_posts(user_id: int, db: Annotated[AsyncSession, Depends(get_db)]):
+    result = await db.execute(select(User).where(User.id == user_id))
+    user = result.scalars().first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+        )
+    result = await db.execute(
+        select(Post)
+        .options(selectinload(Post.author))
+        .where(Post.user_id == user_id)
+        .order_by(Post.date_posted.desc()),
+    )
+    posts = result.scalars().all()
+    return posts
+
+
+@router.patch("/{user_id}", response_model=UserPrivate)
+async def update_user(
+    user_id: int,
+    user_update: UserUpdate,
+    db: Annotated[AsyncSession, Depends(get_db)],
+):
+    result = await db.execute(select(User).where(User.id == user_id))
+    user = result.scalars().first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+        )
+    if (
+        user_update.username is not None
+        and user_update.username.lower() != user.username.lower()
+    ):
+        result = await db.execute(
+            select(User).where(
+                func.lower(User.username) == user_update.username.lower(),
+            ),
+        )
+        existing_user = result.scalars().first()
+        if existing_user:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Username already exists",
+            )
+    if (
+        user_update.email is not None
+        and user_update.email.lower() != user.email.lower()
+    ):
+        result = await db.execute(
+            select(User).where(
+                func.lower(User.email) == user_update.email.lower(),
+            ),
+        )
+        existing_email = result.scalars().first()
+        if existing_email:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Email already registered",
+            )
+
+    if user_update.username is not None:
+        user.username = user_update.username
+    if user_update.email is not None:
+        user.email = user_update.email.lower()
+    if user_update.image_file is not None:
+        user.image_file = user_update.image_file
+
+    await db.commit()
+    await db.refresh(user)
+    return user
+
+@router.delete("/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(user_id: int, db: Annotated[AsyncSession, Depends(get_db)]):
+    result = await db.execute(select(User).where(User.id == user_id))
+    user = result.scalars().first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+        )
+
+    await db.delete(user)
+    await db.commit()
