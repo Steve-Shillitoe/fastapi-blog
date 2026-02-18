815556144317c1c91d556743fd7f0f9749843b88
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2026-02-18 12:45:19 +0000

    Update DB connection string for PostreSQL DB

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
-SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./blog.db"  # blog.db is the database file
+#SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./blog.db"  # blog.db is the database file
+SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://postgres:pgAdmin@localhost:5433/blogdb"
 
 engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL,
